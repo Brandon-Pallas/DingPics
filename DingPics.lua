8912be7dc87e3776fe7b4253a6d2b398eb04cb5d
Brandon Pallas <brandon.pallas@gmail.com>
2019-09-11 13:53:45 +1000

    Add Request for timeplayed

--- DingPics.lua
+++ DingPics.lua
@@ -2,6 +2,8 @@ local frame = CreateFrame("Frame")
 frame:Hide()
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
+	-- Get the /timeplayed just before taking a screenshot
+	RequestTimePlayed()
 	-- wait a bit so the yellow animation appears 300ms seems good
 	C_Timer.After(.3, Screenshot)
 end
