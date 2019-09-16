d76ac2027b52e9457b81d1528f4dead31ce534b6
Brandon Pallas <brandon.pallas@gmail.com>
2019-09-16 22:35:09 +1000

    Changed wait to 200ms

--- DingPics.lua
+++ DingPics.lua
@@ -4,7 +4,7 @@ frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
 	-- Get the /timeplayed just before taking a screenshot
 	RequestTimePlayed()
-	-- wait a bit so the yellow animation appears 300ms seems good
-	C_Timer.After(.3, Screenshot)
+	-- wait a bit so the yellow animation appears 300ms was too long, trying 200ms
+	C_Timer.After(.2, Screenshot)
 end
 )
